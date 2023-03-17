51420f25d8f203c9c082aedd3246d3e410f98209
ulrikson <erik.ulrikson@gmail.com>
2023-03-17 11:08:31 +0100

    creating class

--- todoist.py
+++ todoist.py
@@ -6,57 +6,58 @@ import json
 
 load_dotenv()
 
-INBOX_PROJECT_ID = 2309764563
 
+class Todoist:
+    def __init__(self) -> None:
+        self.inbox_project_id = 2309764563
 
-def get_tasks(params):
-    """Get tasks from Todoist API
-    See https://developer.todoist.com/rest/v2/#get-active-tasks
-    :param params: dict
-    :return: json
-    """
-    url = "https://api.todoist.com/rest/v2/tasks"
-    headers = {"Authorization": f"Bearer {os.getenv('TODOIST_TOKEN')}"}
+    def get_tasks(self, params):
+        """Get tasks from Todoist API
+        See https://developer.todoist.com/rest/v2/#get-active-tasks
+        :param params: dict
+        :return: json
+        """
+        url = "https://api.todoist.com/rest/v2/tasks"
+        headers = {"Authorization": f"Bearer {os.getenv('TODOIST_TOKEN')}"}
 
-    response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=headers, params=params)
 
-    return response.json()
+        return response.json()
 
+    def create_task(self, data):
+        """Create task in Todoist API
+        See https://developer.todoist.com/rest/v2/#create-a-new-task
+        :param data: dict
+        :return: bool
+        """
 
-def create_task(data):
-    """Create task in Todoist API
-    See https://developer.todoist.com/rest/v2/#create-a-new-task
-    :param data: dict
-    :return: bool
-    """
+        url = "https://api.todoist.com/rest/v2/tasks"
+        headers = {
+            "Content-Type": "application/json",
+            "X-Request-Id": str(uuid.uuid4()),
+            "Authorization": f"Bearer {os.getenv('TODOIST_TOKEN')}",
+        }
 
-    url = "https://api.todoist.com/rest/v2/tasks"
-    headers = {
-        "Content-Type": "application/json",
-        "X-Request-Id": str(uuid.uuid4()),
-        "Authorization": f"Bearer {os.getenv('TODOIST_TOKEN')}",
-    }
+        response = requests.post(url, data=json.dumps(data), headers=headers)
 
-    response = requests.post(url, data=json.dumps(data), headers=headers)
+        return True if response.status_code == 200 else False
 
-    return True if response.status_code == 200 else False
+    def update_task(self, data, task_id):
+        """Update task in Todoist API
+        See https://developer.todoist.com/rest/v2/#update-a-task
+        :param data: dict
+        :param task_id: int
+        :return: bool
+        """
 
+        url = "https://api.todoist.com/rest/v2/tasks/" + str(task_id)
+        headers = {
+            "Content-Type": "application/json",
+            "X-Request-Id": str(uuid.uuid4()),
+            "Authorization": f"Bearer {os.getenv('TODOIST_TOKEN')}",
+        }
 
-def update_task(data, task_id):
-    """Update task in Todoist API
-    See https://developer.todoist.com/rest/v2/#update-a-task
-    :param data: dict
-    :param task_id: int
-    :return: bool
-    """
+        response = requests.post(url, data=json.dumps(data), headers=headers)
 
-    url = "https://api.todoist.com/rest/v2/tasks/" + str(task_id)
-    headers = {
-        "Content-Type": "application/json",
-        "X-Request-Id": str(uuid.uuid4()),
-        "Authorization": f"Bearer {os.getenv('TODOIST_TOKEN')}",
-    }
+        return True if response.status_code == 200 else False
 
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-
-    return True if response.status_code == 200 else False
