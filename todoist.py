4cb0e1d18c6ce84a3242fb4705f58bbc771f0a26
ulrikson <erik.ulrikson@gmail.com>
2023-03-17 10:45:47 +0100

    get tasks method

--- todoist.py
+++ todoist.py
@@ -4,9 +4,13 @@ import os
 
 load_dotenv()
 
-url = "https://api.todoist.com/rest/v2/tasks"
-headers = {"Authorization": "Bearer " + os.getenv("TODOIST_TOKEN")}
 
-response = requests.get(url, headers=headers)
+def get_tasks(params):
+    url = "https://api.todoist.com/rest/v2/tasks"
+    headers = {"Authorization": "Bearer " + os.getenv("TODOIST_TOKEN")}
 
-print(response.json())
+    response = requests.get(url, headers=headers, params=params)
+
+    return response.json()
+
+
