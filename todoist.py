1b55c7638b8ec7fc53bdb0a5a2b03f3d63aa84a4
ulrikson <erik.ulrikson@gmail.com>
2023-03-17 10:48:01 +0100

    documentation

--- todoist.py
+++ todoist.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 
 def get_tasks(params):
+    """Get tasks from Todoist API
+    :param params: dict
+    :return: json
+    """
     url = "https://api.todoist.com/rest/v2/tasks"
     headers = {"Authorization": "Bearer " + os.getenv("TODOIST_TOKEN")}
 
