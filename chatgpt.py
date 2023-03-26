209d0d184e2ca01ae3df94eaae02062cd33b94b6
ulrikson <erik.ulrikson@gmail.com>
2023-03-26 12:18:10 +0200

    completion script done

--- chatgpt.py
+++ chatgpt.py
@@ -4,17 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class ChatGPT:
     def __init__(self):
+        self.model = "gpt-3.5-turbo"
         self.api_key = os.getenv("OPENAI_API_KEY")
         openai.api_key = self.api_key
     
     def chat(self, message):
         completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=self.model,
             messages=[
                 {"role": "user", "content": message}
             ]
         )
-        return completion.choices[0].message
+        return completion.choices[0].message.content
--- completion.py
+++ completion.py
@@ -1,8 +1,18 @@
 from todoist import Todoist
+from chatgpt import ChatGPT
 
 todoist = Todoist()
+chatgpt = ChatGPT()
 
 # Get tasks from Todoist API that contain a question mark
 tasks = todoist.get_completion_tasks()
 
-print(tasks)
+for task in tasks:
+    # Get the question from the task
+    question = task["content"]
+
+    # Get the answer from ChatGPT
+    answer = chatgpt.chat(question)
+
+    # Update the task with the answer
+    todoist.update_task({"description": answer}, task["id"])
